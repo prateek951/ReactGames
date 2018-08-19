89e9f7906e1e2395a7cbc249990724ecff5cfc73
Prateek Madaan <31117403+prateek951@users.noreply.github.com>
2018-08-19 19:02:31 +0530

    dummy authenticated added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,26 @@ import { Route } from "react-router-dom";
 import Header from "./components/Header";
 import HomePage from "./components/HomePage";
 import Games from "./Games";
+import { Redirect } from "react-router-dom";
 import ShowGamePage from "./components/ShowGamePage";
 
 class App extends Component {
+  state = {
+    user: {
+      token: "dummy"
+    }
+  };
+  doLogout = () => {
+    const { token } = this.state.user;
+    this.setState({ user: { token: null } });
+  };
   render() {
+    const { token } = this.state.user;
+    const { redirect } = this.state;
     return (
       <div className="ui container">
-        <Header />
+        <Header isAuthenticated={!!token} doLogout={this.doLogout} />
+        <br />
         <Route path="/" exact component={HomePage} />
         <Route path="/games" component={Games} />
         <Route path="/games/:_id" component={ShowGamePage} />
